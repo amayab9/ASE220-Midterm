3c3728d530447e7fbf4bb0eba080ca6e9673c000
Xophin <pamela.lynn.pepper@gmail.com>
2023-03-07 17:51:06 -0500

    Commented stuff on app.js
    Comments added on app.js

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const quotes={
 	documentID:'1079866388960788480',
 	index:function(){
-		document.getElementById('quotes').innerHTML='Loading quotes, please wait...';
+		
 		database.index(quotes.documentID,function(items){
 			document.getElementById('quotes').innerHTML='';
+			// Steps through the array of quotes
 			for(let i=0;i<items.length;i++){
-				let quote=items[i];
+				// finds the specific quote at index location
+				let user=items[i];
+				// declares el as a created Div element.
 				let el=document.createElement('div');
+				// writes HTML to the new el element.
 				el.innerHTML=`<div>
 						<blockquote>
-							<em><a href="detail.html?index=${i}">${quote.quote}</a></em>
+							<em><a href="detail.html?index=${i}">${user.firstName} ${user.lastName}</a></em>
 						</blockquote>
 						${quote.author}
 						<hr />
 					</div>`;
+					// adds new HTML to the index.html page.
 				document.getElementById('quotes').append(el);
 			}
 		});
@@ -23,7 +28,7 @@ const quotes={
 		// TODO: change item.petX to whatever needs to be changed.
 		database.detail(quotes.documentID,index,function(item){
 			document.getElementById('loading').style.display='none';
-			document.getElementByID('pet-photo').innerText=`<div id="petPhoto" class="col">${item.petPhoto}</div>`;
+			document.getElementByID('pet-photo').innerText=`<div id="petPhoto" class="col">${item.pet[i].petPhoto}</div>`;
 			document.getElementById('pet-ident').innerText=`<div id="petID" class="col">ID #: +${item.petID}+</div><div id="petName" class="col">Name: ${item.petName}</div>`;
 			document.getElementById('pet-kind').innerText=`<div id="petSpecies" class="col">Species: ${item.petType}</div><div id="petBreed" class="col">Breed: ${item.petBreed}</div>`;
 			document.getElementByID('pet-stats').innerText=`<div id="petSex" class="col">Sex: ${item.petSex}</div><div id="petAge" class="col">Age: "${item.petAge}</div><div id="petWeight" class="col">Weight: ${item.petWeight}</div>`;
