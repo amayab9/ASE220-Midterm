cac9471f79ce8b21daaa4e2281b37b34f3197d7b
Sarah Riley <sarahriley@Sarahs-MacBook-Pro-2.local>
2023-03-09 18:57:21 -0500

    Have the medDetails.html filling with med info, Still need to paginate medLog and add it to page.

--- assets/medDetail.js
+++ assets/medDetail.js
@@ -1,71 +1,38 @@
 
-// function loadScript(medications) { 
-//     let targetIndex;
-//     // pick the pet based on ID which is passed through query string.
-//     for(let i=0; i < medications.medicationLog.length; i++){
-//         if(medications.medicationLog[i].userID== getAllUrlParams().medications){
-//             if(medications.mediacationLog[i].petID == getAllUrlParams().medications){
-//             if(medications.medicationLog[i].medicationID==getAllUrlParams().medications){
-//             console.log("print found ID:" + medications.medicationLog[i].medicationID);
-//             targetIndex = i;
-//             }
-//         }
-//         }
-//         else{}
-//     }
-// }
-
-// const medications={
-// 	documentID:'1079866388960788480',
-// 	index:function(){
-		
-// 		database.index(medications.documentID,function(items){
-// 			document.getElementById('medications').innerHTML='';
-// 			// Steps through the array of medications
-        
-// 			for(let i=0;i<items.length;i++){
-// 				// finds the specific quote at index location
-// 				let user=items[i];
-// 				// declares el as a created Div element.
-// 				//let el=document.createElement('div');
-// 				// writes HTML to the new medInfo element.
-// 				medInfo.innerHTML += `
-//                 <div id="medName" class="container">
-//                     <h1>${medications.medicationLog[targetIndex].medicationName}</h1>
-//                     <div id="medicationDetails" class="container">
-//                         Medication ID: ${medications.medicationLog[targetIndex].ID}<br />
-//                         Medication Type: ${medications.medicationLog[targetIndex].medType}<br />
-//                         Pet: ${medications.medicationLog[targetIndex].petName}<br />
-//                         Dosage: ${medications.medicationLog[targetIndex].medDosage}<br />
-//                         Medication Notes: ${medications.medicationLog[targetIndex].medNotes}<br />
-//                     </p>
-//                 </div>`;
-// 					// adds new HTML to the index.html page.
-// 				document.getElementById('medications').append(medInfo);
-// 			}
-// 		});
-// 	}
-// }
+// 
 //Function to get json
 async function fetchFunction(){
     // Fetch the JSON 
-    const response = api.GET(medications.documentID, function(response) {
-        console.log(response.data)
-    });
-   
-    // Resolves the promise as JSON
-    
-    // pass content to loadScript for usage
-    loadScript(content);
+    const response = api.GET('1079866388960788480', function(response) {
+        const medicationID = getAllUrlParams().medicationid;
+        let medications= response.data.medications.filter(function (el){
+            return el.medicationID === medicationID;
+        });
+        console.log(medications);
+        const med = medications[0];
+        const pets = response.data.pets.filter(function (pet){
+            return pet.petID === med.petID;
+        });
+        const pet= pets[0];
+        writeToDiv(med,pet);
+    }); 
+}
+// write information to html divs
+function writeToDiv(med, pet){
+    document.getElementById('medInfo').innerHTML +=`
+    <div id="medName" class="container">
+        <h1>${med.medicationName}</h1>
+        <div id="medicationDetails" class="container">
+            Medication ID: ${med.medicationID}<br />
+            Medication Type: ${med.medType}<br />
+            Pet: ${pet.petName}<br />
+            Dosage: ${med.dosage}<br />
+            Medication Notes: ${med.medNotes}<br />
+        
+    </div>`;
 }
 
-// might use this to get the JSON object into pets object.
-function writeToVariable(content1, variable){
-    let parsedJSON;
-    parsedJSON = JSON.parse(content1);
-    console.log("Check for parsed JSON" + parsedJSON);
-    variable = parsedJSON;
-} 
+
 
 
 document.addEventListener('DOMContentLoaded', async () => await fetchFunction(), false);
