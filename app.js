828aafa029c3acc43b8b6f9b2936e118a9b7043b
HPRobbins <haileyrobbins202@gmail.com>
2023-03-09 14:05:49 -0500

    Initial App.js changes

--- app.js
+++ app.js
@@ -1,30 +1,57 @@
 const quotes={
 	documentID:'1079835974602866688',
 	index:function(){
-		document.getElementById('quotes').innerHTML='Loading quotes, please wait...';
-		database.index(quotes.documentID,function(items){
-			document.getElementById('quotes').innerHTML='';
-			for(let i=0;i<items.length;i++){
-				let quote=items[i];
+		
+		database.index(x.documentID,function(items){
+			document.getElementById('items.users').innerHTML='';
+
+			for(let i=0;i<items.users.length;i++){
+
+				let user=items.users[i];
+
 				let el=document.createElement('div');
+
 				el.innerHTML=`<div>
 						<blockquote>
-							<em><a href="detail.html?index=${i}">${quote.quote}</a></em>
+							<em><a href="detail.html?index=${i}">${user.firstName} ${user.lastName}</a></em>
 						</blockquote>
-						${quote.author}
 						<hr />
 					</div>`;
-				document.getElementById('quotes').append(el);
+
+				document.getElementById('items.user').append(el);
 			}
-		});
-	},
+
+			for(let i=0;i<items.pets.length;i++){
+				let pet=items.pets[i];
+
+				let elm=document.createElement('div');
+				elm.innerHTML=`<div>
+				<div col>
+				<div class="card mb-3" style="max-width: 540px;">
+				<div class="row g-0">
+	  			<div class="col-md-4">
+	  			</div>
+				  <div class="col-md-8">
+				  <div class="card-body">
+				  	<h5 class="card-title">${item.petName}</h5>
+					  <p class="card-text">${item.breed}<br />
+					  					   ${item.age}<br />
+										   ${item.location}</p>
+					<p class="card-text"><small class="text-muted">${item.updates}</small></p>
+					<button a href="petDetail.html?index=${item.ID}" type="button" class="btn btn-dark">View</a></button>
+				  </div`
+
+	  }
+				document.getElementById('pets').append(elm);
+			},
+
 	detail:function(index){
 		database.detail(quotes.documentID,index,function(item){
 			document.getElementById('loading').style.display='none';
 			document.getElementById('quote-author').innerText=item.author;
 			document.getElementById('quote-text').innerText=item.quote;
 			document.getElementById('btn-edit').setAttribute('href',`edit.html?index=${index}`);
-							
+
 			let deleteButton=document.getElementById('btn-delete');
 			deleteButton.addEventListener('click',function(){
 				database.delete(quotes.documentID,index);
@@ -48,7 +75,7 @@ const quotes={
 			document.getElementById('loading').style.display='none';
 			document.querySelector('form input[name=author]').value=item.author;
 			document.querySelector('form textarea[name=quote]').value=item.quote;
-			
+
 			document.querySelector('form').addEventListener('submit',function(e){
 				e.preventDefault();
 				let author=document.querySelector('form input[name=author]');
